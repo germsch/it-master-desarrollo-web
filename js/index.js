c8b80c97bea29d8960fa3dc197ac223779816474
germsch <germanschinca@gmail.com>
2021-04-18 16:33:59 -0300

    js y forms

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
-ECHO est� activado.
+var primerNombre = 'Germán'
+var segundoNombre = ''
+var apellido = 'Schinca'
+var edad = 37
+var altura = 1.72
+var esExtranjero = false
+var esMayorDeEdad = edad > 17
+
+
+var nombreCompleto = primerNombre + ' ' + segundoNombre + ' ' + apellido
+
+var cantidadDeLetras = nombreCompleto.length
+console.log (cantidadDeLetras)
+
+if (esMayorDeEdad) {
+    console.log('Puedes acceder al sitio')
+} else {
+    console.log('Te redigiremos a otro sitio')
+}
+
+if ( esExtranjero == true ) {
+    console.log('Bienvenido a nuestro pais')
+} else {
+    console.log('Hola, compatriota')
+}
+
+var cartelCookie = document.querySelector('.cookie')
+var aceptar = document.querySelector('.cookie a')
+var mostrarMensaje = false
+
+if (mostrarMensaje) {
+    cartelCookie.style.display = 'none'
+}
+
+aceptar.addEventListener('click', function () {
+    cartelCookie.style.display = 'none'
+})
+
+var botonDeEnviar = document.querySelector('#footer form button')
+
+botonDeEnviar.addEventListener('click', function () {
+    botonDeEnviar.parentElement.remove()
+    
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = 'Hemos recibido su suscripción'
+
+    document.querySelector('#footer').appendChild(parrafo)
+})
